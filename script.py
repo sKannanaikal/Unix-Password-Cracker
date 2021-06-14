d2e678b73787f19b72c6427614c06ff00eef655a
sKannanaikal <deenathomas@Seans-iMac.fios-router.home>
2021-06-14 14:48:13 -0400

    finished making password cracker script

--- script.py
+++ script.py
@@ -3,30 +3,34 @@ import crypt
 
 def checkPassword(passHash, word):
     salt = passHash[0:2]
-    currentHash = crypt.crypt(word, salt)
 
-    if currentHash is passHash:
+    currentHash = crypt.crypt('nutmeg', 'Mi')
+
+    if currentHash.__eq__(passHash):
         print("Found the password {solution}".format(solution=word))
-        return true
+        return True
     else:
-        print("Continuing to Search...")
-        return false
+        return False
 
 def main():
     if(len(sys.argv) == 3):
-        passwordFile = sys.argv[1]
-        dictionaryFile = sys.argv[2]
+        passwordFile = open(sys.argv[1])
+        dictionaryFile = open(sys.argv[2])
 
-    for line in passwordFile:
-        credentials = line.split(':')
-        userName = credentials[0]
-        passHash = credentials[1]
+        for userDetails in passwordFile:
+            credentials = userDetails.split(':')
+            userName = credentials[0]
+            passHash = credentials[1]
 
-        print("Currently Cracking Password for {user}".format(user=userName))
+            print("Currently Cracking Password for {user}".format(user=userName))
 
-        for line in dictionaryFile:
-            if checkPassword(passHash, line.strip('\n')):
-                break
+            for line in dictionaryFile:
+                if checkPassword(passHash.strip('\n'), line.strip('\n')):
+                    break
+
+            dictionaryFile.seek(0)
+
+    print("Process Finished!")
 
 if __name__ == "__main__":
     main()
